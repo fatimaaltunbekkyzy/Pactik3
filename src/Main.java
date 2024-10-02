8bc426fd7f2ce0fd2dc9b05ecc87c8dacfae00aa
Practik3 <altunbekkuzufatima@gmail.com>
2024-10-02 13:44:49 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*Параметирине бир санды
+        алган жана ал сан оң болсо
+        "Оң сан", терс болсо "Терс сан"
+        деп консолго чыгаруучу метод
+        жазыныз. Void
+*/
+       /* Scanner scanner = new Scanner(System.in);
+        int sun = scanner.nextInt();
+        getSan(sun);
+    }
+    public static void getSan(int san){
+        if(san > 0){//Эгерде биздин параметрдеги саныбыз чон болсо 0 анда он сан деп чыксын
+            System.out.println(" Positive san");
+        }else {//болбосо терс сан деп чыксын
+            System.out.println("Negetive san");
+*/
+
+     /*   Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(getSan(a,b,c));
+    }
+    public static int getSan(int a, int b, int c){//уч санды эсептоо учун метод
+        return (a+b+c)/3;//орточо арифметикалык орточо санын кайтарат
+    }
+    }
+         /*  Task2
+        Үч сандын орточо санын эсептөө
+        үчүн Java метод жазыңыз.
+        Сыноо маалыматтары: Биринчи
+        санды киргизиңиз: 25. Экинчи
+        санды киргизиңиз: 45. Үчүнчү
+        санды киргизиңиз: 65.
+        Күтүлгөн натыйжа:
+        Орточо маани 45.0.
+
+        */
+
+
+    }}
